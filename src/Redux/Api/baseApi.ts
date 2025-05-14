185661103cc101c79fd157bba142b4d8f0c3c31a
Mehedi Mehad <mdmehedihasanmehad@gmail.com>
2025-05-14 17:47:20 +0600

    baseApi

--- src/Redux/Api/baseApi.ts
+++ src/Redux/Api/baseApi.ts
@@ -4,9 +4,9 @@ import Cookies from 'js-cookie';
 export const baseApi = createApi({
     reducerPath: 'baseApi', // The key for this API in the Redux store
     baseQuery: fetchBaseQuery({
-        // baseUrl: 'https://backend.fightnetapp.com/api/v1', 
+        baseUrl: 'https://backend.fightnetapp.com/api/v1', 
         // baseUrl: 'http://localhost:5002//api/v1', 
-        baseUrl: 'http://10.0.20.36:5002/api/v1', 
+        // baseUrl: 'http://10.0.20.36:5002/api/v1', 
         
         prepareHeaders: (headers) => {
             const token = Cookies.get("accessToken") // Assuming token is stored in the auth slice
